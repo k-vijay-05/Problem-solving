cf745522fe80cae5cff1cabcb9eb6b29cc44b223
K VIJAY <146370607+k-vijay-05@users.noreply.github.com>
2024-05-19 13:04:26 +0530

    Time: 67 ms (57.20%), Space: 29.1 MB (53.65%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,14 +1,15 @@
+//TC : O(nlogn)+O(n*n)
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
+        sort(nums.begin(),nums.end()); // O(nlogn)
+        for(int i=0;i<n;i++){ // O(n)
             if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=n-1;
-            while(j<k){
+            while(j<k){  // O(n)
                 int sum=nums[i]+nums[j]+nums[k];
                 if(sum<0){
                     j++;
